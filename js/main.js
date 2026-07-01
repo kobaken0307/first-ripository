c31b244362dc5e1019a0de58a94425079991b71e
kobaken0307 <kobaken945@gmail.com>
2026-07-01 22:13:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
+console.log("JS動いてる");
+
 const fadeElements = document.querySelectorAll(".fade");
-
-const observer = new IntersectionObserver(function(entries){
-
-    entries.forEach(function(entry){
-
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-
-    });
-
-});
+console.log(fadeElements);
 
 fadeElements.forEach(function(element){
-
-    observer.observe(element);
-
+  element.classList.add("show");
 });
