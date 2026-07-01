b1f56377d58ab1219e6f78257c6341dff4d07ffb
kobaken0307 <kobaken945@gmail.com>
2026-07-01 21:36:46 +0900

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = link.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    targetElement.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
