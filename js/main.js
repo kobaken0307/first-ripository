2e7e243e8e8a5947e8638eaaff4e061fc79af58c
kobaken0307 <kobaken945@gmail.com>
2026-07-01 22:15:34 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-console.log("JS動いてる");
-
-const fadeElements = document.querySelectorAll(".fade");
-console.log(fadeElements);
-
-fadeElements.forEach(function(element){
-  element.classList.add("show");
-});
+alert("JS読み込まれてる");
